bad5c5b448fcbfbe5b4366f5fe3994a22d9b4cb5
Kaisercoding <kaisercoding@outlook.com>
2021-03-26 00:37:13 -0400

    fixed button functionality and text output to webpage.

--- Develop/script.js
+++ Develop/script.js
@@ -2,12 +2,64 @@
 var generateBtn = document.querySelector("#generate");
 
 //Arrays to pull from
-var letter = ["a,b,c,d,e,f,g,h,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z"];
-caps = ["A,B,C,D,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,U,V,W,X,Y,Z"];
-numeric = ["0,1,2,3,4,5,6,7,8,9"];
-symbol = ["!,@,#,$,%,&,*,<,>,/,?,+,"];
+var letter = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+caps = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+symbol = ["!", "@", "#", "$", "%", "&", "*", "<", ">", "/", "?", "+"];
 userChoice = [];
-//
+randomPass = [];
+console.log(password);
 
 // Write password to the #password input
 function writePassword() {
@@ -17,12 +69,23 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+writePassword();
 
-function collectInfo() {
+// Add event listener to generate button
+generateBtn.addEventListener("click", function () {
+  writePassword();
+});
+
+function generatePassword() {
   //prompt for pw length
-  var length = prompt("Please enter a number between 8 and 128");
+  var passCount = prompt("Please enter a number between 8 and 128");
+
+  if (passCount >= 8 && length <= 128) {
+    console.log(passCount);
+  } else {
+    prompt("Invalid selection. \n Please enter a number between 8 and 128.");
+    return;
+  }
 
   // prompt for uppercase
   var upper = confirm("Include uppercase letters? Click OK to agree");
@@ -33,63 +96,40 @@ function collectInfo() {
   // prompt for symbols
   var sym = confirm("Include symbols? Click OK to agree");
 
-  console.log(length + upper + lower + num + sym);
+  console.log(passCount + upper + lower + num + sym);
 
   //if statements to follow
-  // if (length > 8 && length < 128) {
 
-  // } else {
-  // }
   if (upper) {
     userChoice = userChoice.concat(caps);
   } else {
-    return;
+    null;
   }
   if (lower) {
     userChoice = userChoice.concat(letter);
   } else {
     null;
   }
-  if (numeric) {
+  if (num) {
     userChoice = userChoice.concat(numeric);
   } else {
     null;
   }
-  if (symbol) {
+  if (sym) {
     userChoice = userChoice.concat(symbol);
   } else {
     null;
   }
-
   console.log(userChoice);
-}
-collectInfo();
-// function password_generator(len) {
-//   var length = len ? len : 16;
-//   var string = "abcdefghijklmnopqrstuvwxyz"; //to upper
-//   var numeric = "0123456789";
-//   var punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
-//   var password = "";
-//   var character = "";
-//   var crunch = true;
-//   while (password.length < length) {
-//     entity1 = Math.ceil(string.length * Math.random() * Math.random());
-//     entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
-//     entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
-//     hold = string.charAt(entity1);
-//     hold = password.length % 2 == 0 ? hold.toUpperCase() : hold;
-//     character += hold;
-//     character += numeric.charAt(entity2);
-//     character += punctuation.charAt(entity3);
-//     password = character;
-//   }
-//   password = password
-//     .split("")
-//     .sort(function () {
-//       return 0.5 - Math.random();
-//     })
-//     .join("");
-//   return password.substr(0, len);
-// }
+  var passString = userChoice.join("");
+  console.log(passString);
+  randomPass = "";
 
-// console.log(password_generator());
+  for (var i = 0; i < passCount; i++) {
+    var scramble = Math.floor(Math.random() * userChoice.length);
+    randomPass = randomPass + userChoice[scramble];
+
+    console.log(randomPass);
+  }
+  return randomPass;
+}

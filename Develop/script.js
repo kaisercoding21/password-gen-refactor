627bd6f296156f1f810e724c9ec25bd4139908ff
Kaisercoding <kaisercoding@outlook.com>
2021-03-29 19:03:29 -0400

    pushed again to see if this fixes the deplyment issue

--- Develop/script.js
+++ Develop/script.js
@@ -59,7 +59,7 @@ numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 symbol = ["!", "@", "#", "$", "%", "&", "*", "<", ">", "/", "?", "+"];
 userChoice = [];
 randomPass = [];
-console.log(password);
+//
 
 // Write password to the #password input
 function writePassword() {
@@ -102,23 +102,15 @@ function generatePassword() {
 
   if (upper) {
     userChoice = userChoice.concat(caps);
-  } else {
-    null;
   }
   if (lower) {
     userChoice = userChoice.concat(letter);
-  } else {
-    null;
   }
   if (num) {
     userChoice = userChoice.concat(numeric);
-  } else {
-    null;
   }
   if (sym) {
     userChoice = userChoice.concat(symbol);
-  } else {
-    null;
   }
   console.log(userChoice);
   var passString = userChoice.join("");
